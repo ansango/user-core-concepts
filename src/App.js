eefbac02f9e5c686961bf2a6f85d162d536ba319
Anibal Santos <anibalsantosgo@gmail.com>
2021-07-17 21:50:28 +0200

    ui base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import AddUser from "./components/Users/AddUser";
 
 function App() {
   return (
     <div className="App">
-
+      <AddUser />
     </div>
   );
 }
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const AddUserHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="text" id="age" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+export default AddUser;
